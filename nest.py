62b5e89a0edcd1b482044e768359c2e67e49aa07
Adam Adamiak 254355 <adamvadamiak@gmail.com>
2022-01-08 20:49:16 +0100

    tweak to nest

--- nest.py
+++ nest.py
@@ -1,13 +1,13 @@
 import random
 
+
 class nest:
 
-    def __init__(self,n):
+    def __init__(self, n, k):
         self.x = 0
-        self.y = 0 
+        self.y = 0
         self.food_stock = n
+        self.ant_ammount = k
 
     def get_cords(self):
-        return self.x,self.y
-
-nest_worker = nest(50)
+        return self.x, self.y
